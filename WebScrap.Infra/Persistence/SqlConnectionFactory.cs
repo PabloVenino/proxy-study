5894e12d8d7423b8a9d085cba9ce2c6c09a8a742
Pablo Venino <pablo.venino@mult-e.com.br>
2024-09-27 14:05:16 -0300

    hot-fix: Fixing the culture info for Decimal parsing

--- WebScrap.Infra/Persistence/SqlConnectionFactory.cs
+++ WebScrap.Infra/Persistence/SqlConnectionFactory.cs
@@ -1,6 +1,7 @@
 ﻿using Dapper;
 using Microsoft.Data.SqlClient;
 using System.Data;
+using System.Globalization;
 using WebScrap.Application.Abstractions;
 using WebScrap.Core.Models;
 
@@ -21,21 +22,18 @@ public sealed class SqlConnectionFactory() : ISqlConnectionFactory
     
     public async Task SaveData(ApiResponseDto data, SqlConnection connection)
     {
-
         var parameters = new
         {
-            id_scrap = new Guid(),
             scrap_name = "Scrap from Magalu",
             proxy_used = "localhost:8080",
             item_category = "eletrodomesticos",
             item_name = data.Data.Product.Title,
-            item_price = decimal.Parse(data.Data.Product.Price.BestPrice),
+            item_price = decimal.Parse(data.Data.Product.Price.BestPrice, CultureInfo.InvariantCulture),
             item_has_discount = true,
-            item_discount = decimal.Parse(data.Data.Product.Price.FullPrice) - decimal.Parse(data.Data.Product.Price.BestPrice),
+            item_discount = decimal.Parse(data.Data.Product.Price.FullPrice, CultureInfo.InvariantCulture) - decimal.Parse(data.Data.Product.Price.BestPrice, CultureInfo.InvariantCulture),
             item_price_observations = data.Data.Product.Installment.PaymentMethodDescription,
             item_rating_count = 9999, // unknown for this example
             item_rating = data.Data.Product.Rating.Score,
-            scrap_last_update = DateTime.UtcNow,
         };
 
         await connection.QueryAsync("scraping_magalu_upsert", param: parameters, commandType: CommandType.StoredProcedure);
