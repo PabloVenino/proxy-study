8ab0cbbec2f087fe9bb3bf46b2b7ffc1dbd99934
Pablo Venino <pablo.venino@mult-e.com.br>
2024-09-27 14:01:53 -0300

    feat: Adding a list of ids from Magalus GraphQL

--- WebScrapApp/WebScraper.cs
+++ WebScrapApp/WebScraper.cs
@@ -19,13 +19,6 @@ public class WebScraper(string connectionString, ISqlConnectionFactory sqlConnec
             var connection = _sqlConnection.CreateConnection(connectionString);
 
             var client = proxyRotator.ScrapeDataWithRandomProxy();
-
-            object payload = new
-            {
-                query = "\n      query(\n          $id: String!\n        ) {\n          product(\n            productRequest: {\n              id: $id\n            }\n          ) {\n            id\n            title\n            reference\n            url\n            image\n            rating {\n              score\n            }\n            price {\n              price\n              fullPrice\n              bestPrice\n            }\n            installment {\n              paymentMethodDescription\n              quantity\n              amount\n              totalAmount\n            }\n          }\n        }\n      ",
-                variables = new { id = "238492300" }
-            };
-
             // For this specific request for Magazine Luiza
             client.DefaultRequestHeaders.Add("sec-ch-ua", "\"Google Chrome\";v=\"129\", \"Not=A?Brand\";v=\"8\", \"Chromium\";v=\"129\"");
             client.DefaultRequestHeaders.Add("sec-ch-ua-mobile", "?0");
@@ -34,32 +27,43 @@ public class WebScraper(string connectionString, ISqlConnectionFactory sqlConnec
             client.DefaultRequestHeaders.Add("sec-fetch-mode", "cors");
             client.DefaultRequestHeaders.Add("sec-fetch-site", "cross-site");
 
-            var response = await client.PostAsJsonAsync<object>(url, payload);
+            string[] ids = ["237277400", "235420100", "238492300", "231217300", "238403400", "231262800", "080260900", "238431700", "237289700"];
 
-
-            var content = await response.Content.ReadAsStringAsync();
-
-            using var responseStream = await response.Content.ReadAsStreamAsync();
-            Stream decompressedStream;
-
-            if (response.Content.Headers.ContentEncoding.Contains("gzip"))
-                decompressedStream = new GZipStream(responseStream, CompressionMode.Decompress);
-            else
-                decompressedStream = responseStream;
-
-            using var reader = new StreamReader(decompressedStream, Encoding.UTF8);
-
-            string jsonResponse = await reader.ReadToEndAsync();
-            Console.WriteLine(jsonResponse);
-
-            JsonSerializerOptions jsonSerializerOptions = new()
+            for (int i = 0; i < ids.Length; i++)
             {
-                PropertyNameCaseInsensitive = true
-            };
+                object payload = new
+                {
+                    query = "\n      query(\n          $id: String!\n        ) {\n          product(\n            productRequest: {\n              id: $id\n            }\n          ) {\n            id\n            title\n            reference\n            url\n            image\n            rating {\n              score\n            }\n            price {\n              price\n              fullPrice\n              bestPrice\n            }\n            installment {\n              paymentMethodDescription\n              quantity\n              amount\n              totalAmount\n            }\n          }\n        }\n      ",
+                    variables = new { id = ids[i] }
+                };
 
-            ApiResponseDto apiResponse = JsonSerializer.Deserialize<ApiResponseDto>(jsonResponse, jsonSerializerOptions);
+                var response = await client.PostAsJsonAsync<object>(url, payload);
 
-            await _sqlConnection.SaveData(apiResponse, connection);
+
+                var content = await response.Content.ReadAsStringAsync();
+
+                using var responseStream = await response.Content.ReadAsStreamAsync();
+                Stream decompressedStream;
+
+                if (response.Content.Headers.ContentEncoding.Contains("gzip"))
+                    decompressedStream = new GZipStream(responseStream, CompressionMode.Decompress);
+                else
+                    decompressedStream = responseStream;
+
+                using var reader = new StreamReader(decompressedStream, Encoding.UTF8);
+
+                string jsonResponse = await reader.ReadToEndAsync();
+                Console.WriteLine(jsonResponse);
+
+                JsonSerializerOptions jsonSerializerOptions = new()
+                {
+                    PropertyNameCaseInsensitive = true
+                };
+
+                ApiResponseDto apiResponse = JsonSerializer.Deserialize<ApiResponseDto>(jsonResponse, jsonSerializerOptions);
+
+                await _sqlConnection.SaveData(apiResponse, connection);
+            }
 
             return;
         } 
